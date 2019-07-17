bde8d00997b411c3a48ed56c1125c31c526bd564
John Ramirez <jvramirez13@gmail.com>
2019-07-17 12:51:19 -0400

    updated interface.js

--- src/Components/Interface.js
+++ src/Components/Interface.js
@@ -29,13 +29,23 @@ const Marker = ({ title }) => (
   </div>
 );
 
+function evaluateBoolean(number){
+  if(number === 0){
+    return "False"
+  }else{
+    return "True"
+  }
+}
+
 class Interface extends Component {
   state = {
     latitude: 0.0,
     longitude: 0.0,
     xaxis: 0.0,
     yaxis: 0.0,
-    zaxis: 0.0
+    zaxis: 0.0,
+    dropped: false,
+    motion: false
   };
 
   componentDidMount() {
@@ -46,14 +56,14 @@ class Interface extends Component {
             process.env.REACT_APP_API_URL
           )
           .then(response => {
-            console.log(response);
-            console.log(response.data.Items[0].Latitude);
             this.setState({
-              latitude: parseFloat(response.data.Items[0].Latitude),
-              longitude: parseFloat(response.data.Items[0].Longitude),
-              xaxis: parseFloat(response.data.Items[0].Xaxis),
-              yaxis: parseFloat(response.data.Items[0].Yaxis),
-              zaxis: parseFloat(response.data.Items[0].Zaxis)
+              latitude: response.data.Latitude,
+              longitude: response.data.Longitude,
+              xaxis: response.data['X-axis'],
+              yaxis: response.data['Y-axis'],
+              zaxis: response.data['Z-axis'],
+              dropped: evaluateBoolean(response.data.Dropped),
+              motion: evaluateBoolean(response.data['Motion Detected'])
             });
           }),
       1000
@@ -67,14 +77,16 @@ class Interface extends Component {
   render() {
     return (
       <div style={{ height: "650px", width: "650px" }}>
-        <h4>Latitude (g): {this.state.latitude}</h4>
-        <h4>Longitude (g): {this.state.longitude}</h4>
+        <h4>Latitude (°): {this.state.latitude}</h4>
+        <h4>Longitude (°): {this.state.longitude}</h4>
         <h4>X-axis (g): {this.state.xaxis}</h4>
         <h4>Y-axis (g): {this.state.yaxis}</h4>
         <h4>Z-axis (g): {this.state.zaxis}</h4>
+        <h4>Dropped: {this.state.dropped}</h4>
+        <h4>Motion Detected: {this.state.motion}</h4>
         <GoogleMap
           bootstrapURLKeys={{
-            key: "AIzaSyALsG7k0wDR46eH66mVKZ6Yu9Fxh5liyLU"
+            key: process.env.REACT_APP_MAPS_KEY
           }}
           center={{ lat: this.state.latitude, lng: this.state.longitude }}
           zoom={14}
