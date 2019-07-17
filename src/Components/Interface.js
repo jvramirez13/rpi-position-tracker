04ca77e1835e84a6912f5e66ef55bfcb2d560237
John Ramirez <jvramirez13@gmail.com>
2019-07-17 15:51:26 -0400

    updated components to react hooks

--- src/Components/Interface.js
+++ src/Components/Interface.js
@@ -1,105 +1,87 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMap from "google-map-react";
 import axios from "axios";
 
+export default function Interface() {
+  // const mapStyles = {
+  //   width: "100%",
+  //   height: "100%"
+  // };
 
-// const mapStyles = {
-//   width: "100%",
-//   height: "100%"
-// };
+  const [latitude, setLatitude] = useState(0.0);
+  const [longitude, setLongitude] = useState(0.0);
+  const [xaxis, setXaxis] = useState(0.0);
+  const [yaxis, setYaxis] = useState(0.0);
+  const [zaxis, setZaxis] = useState(0.0);
+  const [dropped, setDropped] = useState("");
+  const [motion, setMotion] = useState("");
 
-const markerStyle = {
-  height: "50px",
-  width: "50px",
-  marginTop: "-50px"
-};
-
-const imgStyle = {
-  height: "100%"
-};
-
-const Marker = ({ title }) => (
-  <div style={markerStyle}>
-    <img
-      style={imgStyle}
-      src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png"
-      alt={title}
-    />
-    <h3>{title}</h3>
-  </div>
-);
-
-function evaluateBoolean(number){
-  if(number === 0){
-    return "False"
-  }else{
-    return "True"
-  }
-}
-
-class Interface extends Component {
-  state = {
-    latitude: 0.0,
-    longitude: 0.0,
-    xaxis: 0.0,
-    yaxis: 0.0,
-    zaxis: 0.0,
-    dropped: false,
-    motion: false
+  const markerStyle = {
+    height: "50px",
+    width: "50px",
+    marginTop: "-50px"
   };
 
-  componentDidMount() {
-    this.interval = setInterval(
+  const imgStyle = {
+    height: "100%"
+  };
+
+  const Marker = ({ title }) => (
+    <div style={markerStyle}>
+      <img
+        style={imgStyle}
+        src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png"
+        alt={title}
+      />
+      <h3>{title}</h3>
+    </div>
+  );
+
+  function evaluateBoolean(number) {
+    if (number === 0) {
+      return "False";
+    } else {
+      return "True";
+    }
+  }
+
+  useEffect(() => {
+    let interval = setInterval(
       () =>
-        axios
-          .get(
-            process.env.REACT_APP_API_URL
-          )
-          .then(response => {
-            this.setState({
-              latitude: response.data.Latitude,
-              longitude: response.data.Longitude,
-              xaxis: response.data['X-axis'],
-              yaxis: response.data['Y-axis'],
-              zaxis: response.data['Z-axis'],
-              dropped: evaluateBoolean(response.data.Dropped),
-              motion: evaluateBoolean(response.data['Motion Detected'])
-            });
-          }),
+        axios.get(process.env.REACT_APP_API_URL).then(response => {
+          setLatitude(response.data.Latitude);
+          setLongitude(response.data.Longitude);
+          setXaxis(response.data["X-axis"]);
+          setYaxis(response.data["Y-axis"]);
+          setZaxis(response.data["Z-axis"]);
+          setDropped(evaluateBoolean(response.data["Dropped"]));
+          setMotion(evaluateBoolean(response.data["Motion Detected"]));
+        }),
       1000
     );
-  }
+    return () => {
+      clearInterval(interval);
+    };
+  });
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <div style={{ height: "650px", width: "650px" }}>
-        <h4>Latitude (°): {this.state.latitude}</h4>
-        <h4>Longitude (°): {this.state.longitude}</h4>
-        <h4>X-axis (g): {this.state.xaxis}</h4>
-        <h4>Y-axis (g): {this.state.yaxis}</h4>
-        <h4>Z-axis (g): {this.state.zaxis}</h4>
-        <h4>Dropped: {this.state.dropped}</h4>
-        <h4>Motion Detected: {this.state.motion}</h4>
-        <GoogleMap
-          bootstrapURLKeys={{
-            key: process.env.REACT_APP_MAPS_KEY
-          }}
-          center={{ lat: this.state.latitude, lng: this.state.longitude }}
-          zoom={14}
-        >
-          <Marker
-            title={"Current Location"}
-            lat={this.state.latitude}
-            lng={this.state.longitude}
-          />
-        </GoogleMap>
-      </div>
-    );
-  }
+  return (
+    <div style={{ height: "650px", width: "650px" }}>
+      <h4>Latitude (°): {latitude}</h4>
+      <h4>Longitude (°): {longitude}</h4>
+      <h4>X-axis (g): {xaxis}</h4>
+      <h4>Y-axis (g): {yaxis}</h4>
+      <h4>Z-axis (g): {zaxis}</h4>
+      <h4>Dropped: {dropped}</h4>
+      <h4>Motion Detected: {motion}</h4>
+      <GoogleMap
+        bootstrapURLKeys={{
+          key: process.env.REACT_APP_MAPS_KEY
+        }}
+        center={{ lat: latitude, lng: longitude }}
+        zoom={14}
+      >
+        <Marker title={"Current Location"} lat={latitude} lng={longitude} />
+      </GoogleMap>
+    </div>
+  );
 }
-
-export default Interface;
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Title extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Raspberry Pi Tracker</h1>
-      </div>
-    );
-  }
+export default function Title() {
+  return (
+    <div>
+      <h1>Raspberry Pi Tracker</h1>
+    </div>
+  );
 }
-
-export default Title;
